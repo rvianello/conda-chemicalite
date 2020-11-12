9341f7c95fb2580854455695b9f55678d7b702e7
Riccardo Vianello <riccardo.vianello@gmail.com>
2020-11-12 23:46:58 +0100

    complete the cmake config and installation steps

--- chemicalite/build.sh
+++ chemicalite/build.sh
@@ -1,3 +1,11 @@
-cmake $SRC_DIR
+cmake \
+    -D CMAKE_INSTALL_PREFIX=$PREFIX \
+    -D CMAKE_INSTALL_LIBDIR=lib \
+    -D CMAKE_BUILD_TYPE=Release \
+    $SRC_DIR
+
 make
+
 LD_LIBRARY_PATH=$PWD/src ctest
+
+make install
